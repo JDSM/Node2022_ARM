d82d0bbf59177e237675a0b0be40fc959adb6df1
Jesus Daniel Serrano <jesusdmalave@gmail.com>
2022-11-03 17:28:44 -0500

    Adactar el config para conectar los datos a la BD

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,12 @@
 //const store = require('../../../store/postgres')
-const store = require('../../../store/remote-postgres')
+const config = require('../../../config')
+let store
+
+if (config.remoteDB === true) {
+    store = require('../../../store/remote-postgres')
+} else {
+    store = require('../../../store/postgres')
+}
 const controller = require('./controller')
 
 module.exports = controller(store)
